d3d882955a194e49b9083a011f06440c4b6db886
joshuawilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2020-11-26 13:07:17 +0800

    checnged

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ from setuptools import setup, find_packages
 setup(name='compute-rhino3d',
       version='0.0.1',
       description='Patched version',
-      author='Mcneil',
-      packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+      packages=["compute_rhino3d"],
       license='',
     )
